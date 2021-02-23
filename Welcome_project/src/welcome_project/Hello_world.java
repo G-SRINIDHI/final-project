c4cc47ab3f5d8454ec0ccd47a5f4d96ffebc32dd
user <user@DESKTOP-BAV4A2U>
2021-02-23 09:44:51 -0800

    java welcome project

---
+++ Welcome_project/src/welcome_project/Hello_world.java
@@ -0,0 +1,15 @@
+package welcome_project;
+import  java.util.*;
+public class Hello_world 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("welcome to the hard working world");
+		System.out.println("welcome to the hard working world");
+		
+
+	}
+
+}
